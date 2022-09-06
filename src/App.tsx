878707bef2c55283961063c5a73d0df454f5d1b2
Caroline Ponzini <cara@Carolines-MacBook-Air.local>
2022-09-06 11:43:59 -0700

    move globe into component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Globe from 'react-globe.gl';
+import countries_data from './countries.json';
+
+const World = () => {
+  const [countries, setCountries] = useState<{ features: any[] } | null>(null);
+
+  useEffect(() => {
+    // load data
+    // fetch('./countries.json').then(res => res.json()).then((data) => setCountries(data));
+    setCountries(countries_data);
+    console.log('loaded countries', countries)
+  }, [countries]);
+
+  return countries && <Globe
+    width={500}
+    height={500}
+    hexPolygonsData={countries.features}
+    hexPolygonResolution={3}
+    hexPolygonMargin={0.3}
+    hexPolygonColor={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`}
+    // @ts-ignore no-implicit-any
+    hexPolygonLabel={(({ properties: d }) => `
+      <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+      Population: <i>${d.POP_EST}</i>
+    `) as unknown as (obj: object) => string}
+  />;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <World></World>
+      </div>
     </div>
   );
 }
